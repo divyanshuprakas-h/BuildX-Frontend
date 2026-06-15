db2a2c1832671c17fa552b8d14dba43bb53ddb70
Rithull Raj Kallat <rk@RKs-MacBook-Pro.local>
2026-06-15 15:51:30 +0530

    BuildX

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/landing/hero/hero';
+import Pref from './components/landing/pref/pref';
 
 const App = () => {
   return(
     <>
     <Navbar />
     <Hero />
+    <Pref />
     </>
   )
 }
